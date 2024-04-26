179e61b9f85bfd7b914d92cdb0fe11bcabf83161
Madhu <mpriya1201@gmail.com>
2024-04-26 13:19:13 +0200

    added new features

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+
+  mybutton = document.getElementById("myBtn");
+
+function scrollFunction() {
+  console.log(document.body.scrollTop )
+  console.log(document.documentElement.scrollTop)
+  
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+    mybutton.style.display = "block";
+  else 
+    mybutton.style.display = "none";
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+window.onscroll = function() {
+  scrollFunction()
+}
+
+scrollFunction();
 }
